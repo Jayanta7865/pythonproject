b616eaac15047229a61ed731dfc1710b815c789f
Jayanta7865 <jsamanta2003@gmail.com>
2025-07-07 13:07:02 +0530

    first commit

--- task2.py
+++
@@ -1,7 +0,0 @@
-#write a python program that collects multiple type of dara(ex: name ,age,height,student) from user input ,store them in a dictionary and print out the collect data!
-user_data = {}
-user_data ['name']=input("enter your name:")
-user_data ['age']=int(input("enter your age:"))
-user_data ['height']=float(input("enter your height:"))
-user_data ['student']=input("Are you student(yes/no):")
-print(user_data)
