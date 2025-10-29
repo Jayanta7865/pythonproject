2c7017bd8bcc10fa8ccfec10c6dcb0efdac7cad6
Jayanta7865 <jsamanta2003@gmail.com>
2025-10-29 10:19:34 +0530

    task1

---
+++ qrpayment.py
@@ -0,0 +1,17 @@
+import qrcode
+upi_id=input("Enter your UPI ID: ")
+phonepe_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+gpay_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+BHIM_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+phonepe_qr=qrcode.make(phonepe_url)
+gpay_qr=qrcode.make(gpay_url)
+BHIM_qr=qrcode.make(BHIM_url)
+phonepe_qr.save("phonepe_qr.png")
+gpay_qr.save("Gpay_qr.png")
+BHIM_qr.save("BHIm_qr.png")
+phonepe_qr.show()
+gpay_qr.show()
+BHIM_qr.show()
+print("\nPlease scan the QR code using any UPI app...")
+
+
